e6d4a4c7fe0fca1bbc36732d53d96e62cf19a49a
Novirma <raihannovirma@gmail.com>
2023-07-17 23:26:00 +0700

    Edit main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  await app.listen(port, () => {
+    console.log(`Server sedang berjalan di port ${port}`);
+    
+  });
 }
 bootstrap();
